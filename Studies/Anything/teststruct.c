c6cd6d3b5f0379aa2a11bb1474a5a61567f8ffd5
Manoel Pedro <manoeltelesdev@gmail.com>
2024-09-21 12:41:50 -0300

    Update teststruct.c

--- Studies/Anything/teststruct.c
+++ Studies/Anything/teststruct.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
-int main (void){
 
+int main(void) {
     struct horario {
         int horas;
         int minutos;
         int segundos;
-    }
-
+    }; 
+    
     struct horario agora;
     agora.horas = 17;
     agora.minutos = 18;
     agora.segundos = 54; 
 
-
-    printf("A hora agora e %d\n", agora.horas);
-    printf("A hora agora e %d\n", agora.minutos);
-    printf("A hora agora e %d\n", agora.segundos);
-
-
+    printf("A hora agora e %d horas\n", agora.horas);
+    printf("A hora agora e %d minutos\n", agora.minutos);
+    printf("A hora agora e %d segundos\n", agora.segundos);
 
     return 0;
 }
